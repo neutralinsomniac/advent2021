86f6fc1f26b64296e9c159a51a8343943670a6c9
Jeremy O'Brien <neutral@fastmail.com>
2021-12-10 07:45:38 -0500

    day 10 complete

---
+++ 10/10-pt1.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"bufio"
+	"container/list"
+	"fmt"
+	"os"
+	"time"
+)
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+type ByteStack struct {
+	stack *list.List
+}
+
+func (s *ByteStack) Push(val rune) {
+	s.stack.PushFront(val)
+}
+
+func (s *ByteStack) Pop() error {
+	if s.stack.Len() > 0 {
+		ele := s.stack.Front()
+		s.stack.Remove(ele)
+		return nil
+	}
+	return fmt.Errorf("stack empty")
+}
+
+func (s *ByteStack) Front() (rune, error) {
+	if s.stack.Len() > 0 {
+		if val, ok := s.stack.Front().Value.(rune); ok {
+			return val, nil
+		}
+		return 0, fmt.Errorf("Peep Error: Stack Datatype is incorrect")
+	}
+	return 0, fmt.Errorf("Peep Error: Stack is empty")
+}
+
+func (s *ByteStack) Len() int {
+	return s.stack.Len()
+}
+
+func (s *ByteStack) Init() {
+	if s.stack == nil {
+		s.stack = list.New()
+	}
+}
+
+var parensMap = map[rune]rune{
+	'(': ')',
+	'[': ']',
+	'{': '}',
+	'<': '>',
+}
+
+var scoreMap = map[rune]int{
+	')': 3,
+	']': 57,
+	'}': 1197,
+	'>': 25137,
+}
+
+func main() {
+	start := time.Now()
+	file, err := os.Open("input")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	score := 0
+
+NextLine:
+	for scanner.Scan() {
+		row := scanner.Text()
+		var s ByteStack
+		s.Init()
+		lineScore := 0
+		for _, c := range row {
+			/* opening char */
+			if c == '(' || c == '[' || c == '{' || c == '<' {
+				s.Push(c)
+			} else {
+				/* closing char */
+				openingParen, err := s.Front()
+				if err != nil {
+					fmt.Println(err)
+					continue NextLine
+				}
+				if lineScore == 0 && parensMap[openingParen] != c {
+					score += scoreMap[c]
+					continue NextLine
+				}
+				s.Pop()
+			}
+		}
+	}
+
+	fmt.Println(score)
+	fmt.Println("time:", time.Since(start))
+}
